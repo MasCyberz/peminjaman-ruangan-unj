be8615b59ff3b33adf608156741deabb6423033e
robeann <robinmndra12@gmail.com>
2024-03-05 15:14:34 +0700

    mengecek indeks pertama dari data referensi

--- resources/views/admin/ruangan/data-referensi.blade.php
+++ resources/views/admin/ruangan/data-referensi.blade.php
@@ -3,44 +3,27 @@
 @section('page', 'Data Referensi')
 
 @section('content')
+
     <div class="w-[calc(100%-8rem)] h-full mx-auto py-3 px-4">
+        @php
+            $tombolDitampilkan = false;
+        @endphp
 
-        {{-- @foreach ($ruanganList as $ruangan )
-            @foreach ($ruangan as $ruang)
-                {{$ruang->fasilitas->nama_fasilitas}}
-            @endforeach
-        @endforeach --}}
-
-        {{-- @foreach ($ruanganList as $ruangan)
-            @foreach ($ruangan-> as )
-
-            @endforeach
-        @endforeach --}}
-
-        {{-- {{$ruanganList}}
-        <br><br>
-        @foreach ($fasilitasList as $fasilitas )
-            {{$fasilitas->nama_fasilitas}}
-        @endforeach
-            <br><br>
-        @foreach ($ruanganList as $ruang )
-                @foreach ($ruang->fasilitas as $fasilitas )
-                    <span>{{$fasilitas->nama_fasilitas}}</span>
-                    <span>{{$fasilitas->jumlah}}</span>
-                @endforeach
-        @endforeach --}}
-
-        @foreach ($ruanganList as $ruang)
+        @foreach ($ruanganList as $index => $ruang)
             <div class="my-7 mx-auto rounded-lg shadow-all-side h-[420px] w-[80%] flex flex-col p-3 relative">
-                @if ($ruang->nomor_ruang == 307)
+                @if (!$tombolDitampilkan && $index === 0)
+                    @php
+                        $tombolDitampilkan = true;
+                    @endphp
                     <div
                         class="absolute w-14 h-11 inline-flex items-center justify-center left-0 top-0 transition-all duration-300 ease-in-out">
                         <a href="{{ route('tambah_ruangan') }}"><i data-tooltip-target="tooltip-right"
                                 data-tooltip-placement="right"
                                 class='bx bx-plus text-4xl hover:bg-sidebarunj h-11 py-1 rounded-b-lg text-sidebarunj font-medium hover:text-white transition-all duration-300 ease-in-out'></i></a>
                     </div>
-                @else
                 @endif
+
+                
 
                 <div class="flex my-auto justify-center relative">
                     <div class="flex flex-col z-10">
@@ -64,13 +47,13 @@
                         <span class="mt-3 text-base font-semibold mx-3">{{ $ruang->kapasitas_orang }}</span>
                         <span class="mt-3 text-base font-semibold mx-3">
                             <ul>
-                                @foreach ($ruang->fasilitas as $fasilitas )
-                                @if ($ruang->fasilitas && $fasilitas->nama_fasilitas && $fasilitas->jumlah)
-                                <li> <span>{{$fasilitas->jumlah}}</span> <span>{{$fasilitas->nama_fasilitas}}</span> </li>
-                                @else
-                                -
-                                @endif
-
+                                @foreach ($ruang->fasilitas as $fasilitas)
+                                    @if ($ruang->fasilitas && $fasilitas->nama_fasilitas && $fasilitas->jumlah)
+                                        <li> <span>{{ $fasilitas->jumlah }}</span>
+                                            <span>{{ $fasilitas->nama_fasilitas }}</span> </li>
+                                    @else
+                                        -
+                                    @endif
                                 @endforeach
                             </ul>
                         </span>
@@ -90,12 +73,6 @@
                 </div>
             </div>
         @endforeach
-    </div>
-
-    <div id="tooltip-right" role="tooltip"
-        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-sidebarunj rounded-lg shadow-sm opacity-0 tooltip">
-        Tambahkan Ruangan
-        <div class="tooltip-arrow" data-popper-arrow></div>
     </div>
 
 
